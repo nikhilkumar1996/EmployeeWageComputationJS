b12a9a75c7e0e661db87a8abb188344768594a81
nikhilkumar1996 <nikhilkumar1097@gmail.com>
2022-03-22 06:18:12 +0530

    [Nikhil Kumar]:Refactor Code with function

--- EmpWageComputation.js
+++ EmpWageComputation.js
@@ -1,33 +1,21 @@
-{
-    const isPRESENT = 1
-    let empCheck = Math.floor(Math.random() * 10) % 2;
-    if(empCheck == isPRESENT)
-    {
-    console.log("Employee is Present");
-    }
-    else
-    {
-    console.log("Employee is Absent");
-    }
-}
-
 const isPartime   =1;
 const isFullTime  =2;
 const PartimeHrs  =4;
 const FullTimeHrs =8;
 const WagePerHrs  =20;
 
-let empHrs = 0;
-let empCheck = Math.floor(Math.random()*10)%3;
-switch (empCheck){
-    case isPartime:
-        empHrs=PartimeHrs;
-        break;
-    case isFullTime:
-        empHrs=FullTimeHrs;
-        break;
-    default:
-        empHrs=0;
+function getEmpHrs(empCheck){
+    switch (empCheck){
+        case isPartime:
+            return PartimeHrs;
+        case isFullTime:
+            return FullTimeHrs;
+        default:
+            return 0;
+    }
+
 }
+let empCheck = Math.floor(Math.random()*10)%3;
+let empHrs =getEmpHrs(empCheck);
 let empWage=empHrs*WagePerHrs;
 console.log("Employee Wage:",empWage);
