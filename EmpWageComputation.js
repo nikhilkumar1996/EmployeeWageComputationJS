143386c96b2accb9ec3c64db08377523f65172e1
nikhilkumar1996 <nikhilkumar1097@gmail.com>
2022-03-22 05:57:03 +0530

    [Nikhil Kumar]:EmployeeWage is Calulated for a day

--- EmpWageComputation.js
+++ EmpWageComputation.js
@@ -1,10 +1,33 @@
-const isPRESENT = 1
-let empCheck = Math.floor(Math.random * 10) % 2;
-if(empCheck == isPRESENT)
 {
+    const isPRESENT = 1
+    let empCheck = Math.floor(Math.random() * 10) % 2;
+    if(empCheck == isPRESENT)
+    {
     console.log("Employee is Present");
-}
-else
-{
+    }
+    else
+    {
     console.log("Employee is Absent");
+    }
 }
+
+const isPartime   =1;
+const isFullTime  =2;
+const PartimeHrs  =4;
+const FullTimeHrs =8;
+const WagePerHrs  =20;
+
+let empHrs = 0;
+let empCheck = Math.floor(Math.random()*10)%3;
+switch (empCheck){
+    case isPartime:
+        empHrs=PartimeHrs;
+        break;
+    case isFullTime:
+        empHrs=FullTimeHrs;
+        break;
+    default:
+        empHrs=0;
+}
+let empWage=empHrs*WagePerHrs;
+console.log("Employee Wage:",empWage);
